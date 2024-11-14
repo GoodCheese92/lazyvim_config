c744d61a98bdf4ba2728efcceb9c724b2fe77dc3
goodcheese <dltjdwls555@gmail.con>
2024-11-14 10:03:58 +0900

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+      color_overrides = {
+        mocha = {
+          --        base = "#111622",
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
