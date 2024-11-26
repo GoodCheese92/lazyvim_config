0d224286438d93c0f54158a7ebf80c33193790b1
goodcheese <dltjdwls555@gmail.con>
2024-11-26 16:39:41 +0900

    cmp 창 설정 변경

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,40 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
-local cmp = require("cmp")
 
 -- windows
 map("n", "\\", "<C-W>s", { desc = "Split Window Below", remap = true })
 map("n", "|", "<C-W>v", { desc = "Split Window Right", remap = true })
-
--- cmp
-cmp.setup({
-  mapping = {
-    -- 자동완성 창 열기
-    ["<C-Space>"] = cmp.mapping.complete(),
-
-    -- 자동완성 항목 선택
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-
-    -- Tab으로 다음 항목 선택
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback() -- 기본 Tab 동작
-      end
-    end, { "i", "s" }),
-
-    -- Shift+Tab으로 이전 항목 선택
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback() -- 기본 Shift+Tab 동작
-      end
-    end, { "i", "s" }),
-
-    -- 자동완성 창 닫기
-    ["<Esc>"] = cmp.mapping.close(),
-  },
-})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,14 @@ return {
     opts = {
       flavour = "mocha",
       transparent_background = true,
+      default_integrations = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+      },
       color_overrides = {
         mocha = {
           lineNr = { fg = "#A6A6A6" },
@@ -13,6 +21,17 @@ return {
           visual = { bg = "#FFFFD2", fg = "#008000" },
         },
       },
+      custom_highlights = function(colors)
+        return {
+          -- 자동완성 팝업 창 배경 및 텍스트
+          Pmenu = { bg = colors.surface0, fg = colors.text },
+          PmenuSel = { bg = colors.blue, fg = colors.text }, -- 선택된 항목
+          PmenuThumb = { bg = colors.overlay0 }, -- 스크롤바
+          -- 문서 팝업 창
+          NormalFloat = { bg = colors.surface0 },
+          FloatBorder = { bg = colors.surface0, fg = colors.blue },
+        }
+      end,
     },
   },
   {
